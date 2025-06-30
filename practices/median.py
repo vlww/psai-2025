8f033eaa7ec984dc22ea9c26ff7a2828f64e1656
vlww <victorcalehr18@gmail.com>
2025-06-30 14:05:47 -0400

    median

--- practices/median.py
+++ practices/median.py
@@ -4,7 +4,6 @@ def median(numbers):
     sorted_nums = sorted(numbers)
     n = len(sorted_nums)
     mid = n // 2
-
     if n % 2 == 0:
         return (sorted_nums[mid - 1] + sorted_nums[mid]) / 2
     else:
@@ -14,5 +13,4 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("nums", nargs='+', type=float, help="List of numbers to compute the median of")
     args = parser.parse_args()
-
-    print("Median: ", median(args.nums))
+    print(median(args.nums))
