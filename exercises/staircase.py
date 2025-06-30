18a9c70f982f3c76bb9ffc3ba600e5ad17226f0d
vlww <victorcalehr18@gmail.com>
2025-06-30 16:02:27 -0400

    finished staircase

--- exercises/staircase.py
+++ exercises/staircase.py
@@ -1,15 +1,18 @@
 import argparse 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("num", type=int, help="Number of stairs")
-    args = parser.parse_args()
-    for i in range(args.num):
-        for j in range(args.num-i-1):
+def staircase(n):
+    for i in range(n):
+        for j in range(n-i-1):
             print(" ", end='')
         for k in range(i+1):
             print("#", end='')
         print("")
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("n", type=int, help="the size of the staircase")
+    args = parser.parse_args()
+    staircase(args.n)
 
 
 if __name__ == "__main__":
