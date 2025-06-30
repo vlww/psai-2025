d4f479272e11de7fbfe57e8c2100d9deb370012d
vlww <victorcalehr18@gmail.com>
2025-06-30 15:30:22 -0400

    lowercase and split

--- practices/unique_words.py
+++ practices/unique_words.py
@@ -6,17 +6,31 @@ def main():
     parser.add_argument("sentence", type=str, help="Sentence to be split into unique words")
     args = parser.parse_args()
     words = split(args.sentence)
+    words = lower(words)
     words = unique(words)
     print(words)
     
-def split(word):
-    return re.split(r'[^a-zA-Z]+', word)
+def split(sentence):
+    word_list = re.split(r'[^a-zA-Z]+', sentence)
+    words = []
+    for word in word_list:
+        if word:
+            words.append(word)
+    return words
+
+def lower(words):
+    new_list = [0] * len(words)
+    for i in range(len(words)):
+        new_list[i] = words[i].lower()
+    return new_list
 
 def unique(words):
-    unique_words = set()
+    seen = set()
+    unique_words = []
     for word in words:
-        if word:
-            unique_words.add(word.lower())
+        if word not in seen:
+            seen.add(word.lower())
+            unique_words.append(word)
     return unique_words
 
 if __name__ == "__main__":
