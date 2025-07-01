0b870e811d3e6b2b93d7657107aefbe60ba0aa69
vlww <victorcalehr18@gmail.com>
2025-07-01 13:47:59 -0400

    print state

--- practices/cracker_barrel.py
+++ practices/cracker_barrel.py
@@ -1,5 +1,18 @@
 def cracker_barrel():
-    print("a")
+    positions = [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,0,0,1]
+    print_state(positions, 6)
+
+def print_state(list, size):
+    position = 0
+    for i in range(1, size+1):
+        print(" "*(size-i), end="")
+        for j in range(i):
+            if list[position] == 0:
+                print("O ", end="")
+            if list[position] == 1:
+                print("# ", end="")
+            position += 1
+        print()
 
 def main():
     cracker_barrel()
