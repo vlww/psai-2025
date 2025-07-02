f1458cb8b9b56baac1b955b6d222cf46451d9438
vlww <victorcalehr18@gmail.com>
2025-07-02 13:03:18 -0400

    bfs visits all nodes

--- practices/bfs.py
+++ practices/bfs.py
@@ -1,10 +1,32 @@
-def bfs(filename):
+def bfs(start, goal):
+    adjacency_list = {}
+    filename = "practices/graph.txt"
     try:
         with open(filename, 'r') as file:
-            unsorted_list = [int(line.strip()) for line in file if line.strip() != '']
+            for line in file:
+                split_list = line.strip().split(" ")
+                parent = split_list[0]
+                children = split_list[1:]
+                adjacency_list[parent] = children
     except Exception as e:
         print(f"Failed to read file '{filename}': {e}")
         return
 
+    queue = [start]
+    visited = set([start])
+    visited_list = []
+
+    while queue:
+        current = queue.pop(0)
+        visited_list.append(current)
+        if current == goal:
+            break
+        for neighbor in adjacency_list.get(current, []):
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append(neighbor)
+
+    return visited_list
+
 if __name__ == '__main__':
-    print(bfs('practices/list.txt'))
+    print(bfs("A", "H"))
