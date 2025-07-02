36c619cc6f8f6036e43b873a9fef2696b1dcab64
vlww <victorcalehr18@gmail.com>
2025-07-02 09:35:24 -0400

    list.txt

--- practices/peer_programming.py
+++ practices/peer_programming.py
@@ -1,14 +1,22 @@
 import argparse
 
-def find_index(l):
-    splitter, m = l.rsplit(" ", 1)
-    clean = splitter.strip("[]")
-    stuff = clean.split(",")
-    listt = [int(item.strip()) for item in stuff if item.strip() != '']  # Remove blanks
-    n = int(m.strip())
+def find_index_from_file(filename, target):
+    try:
+        with open(filename, 'r') as file:
+            # Read all lines and convert to integers
+            listt = [int(line.strip()) for line in file if line.strip() != '']
+    except Exception as e:
+        print(f"Failed to read file '{filename}': {e}")
+        return -1
 
-    for i in range(len(listt)):
-        if listt[i] == n:
+    try:
+        target = int(target)
+    except ValueError:
+        print(f"Target must be an integer, got '{target}'")
+        return -1
+
+    for i, value in enumerate(listt):
+        if value == target:
             print(i)
             return i
     print(-1)
@@ -16,6 +24,6 @@ def find_index(l):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('s', type=str, help='the string to process')
+    parser.add_argument('target', type=str, help='Target number to find in list.txt')
     args = parser.parse_args()
-    find_index(args.s)
+    find_index_from_file('practices/list.txt', args.target)
