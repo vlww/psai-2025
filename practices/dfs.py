6717c69e197aca539515f92d50a25d120b2c29f9
vlww <victorcalehr18@gmail.com>
2025-07-03 10:31:29 -0400

    working dfs

---
+++ practices/dfs.py
@@ -0,0 +1,40 @@
+import argparse
+
+def dfs(start, goal):
+    adjacency_list = {}
+    filename = "practices/graph.txt"
+    try:
+        with open(filename, 'r') as file:
+            for line in file:
+                split_list = line.strip().split(" ")
+                parent = split_list[0]
+                children = split_list[1:]
+                adjacency_list[parent] = children
+    except Exception as e:
+        print(f"Failed to read file '{filename}': {e}")
+        return
+
+    queue = [start]
+    visited = set([start])
+    visited_list = []
+
+    while queue:
+        print(queue)
+        current = queue.pop(0)
+        visited_list.append(current)
+        if current == goal:
+            break
+        for neighbor in reversed(adjacency_list.get(current, [])):
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.insert(0, neighbor)
+
+                
+    return visited_list
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('start', type=str, help='Start node')
+    parser.add_argument('goal', type=str, help='Goal node')
+    args = parser.parse_args()
+    print(dfs(args.start, args.goal))
