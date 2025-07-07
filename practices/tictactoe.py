3ee62db646ad20ec6e569536c652ca9d457e77dc
vlww <victorcalehr18@gmail.com>
2025-07-07 09:42:54 -0400

    fix print

--- practices/tictactoe.py
+++ practices/tictactoe.py
@@ -1,27 +1,33 @@
+import argparse
+
 def main():
     values = create_board()
     print_board(values)
 
 def create_board():
-    values = [0, 0, 1, 0, 0, 2, 0, 0, 0]
+    values = [2, 0, 1, 0, 0, 2, 0, 0, 0]
     return values
 
 def print_board(values):
-    for i in range(9):
-        if values[i] == 0:
-            print(" ", end="")
-        if values[i] == 1:
-            print("X", end="")
-        if values[i] == 2:
-            print("O", end="")
-        if not i%3==0:
-            print("|", end="")
-        if i%3==2:
-            print()
-            if not i==8:
-                print("-------")
-       
+    for row in range(3):
+        row_str = " "
+        for col in range(3):
+            i = row * 3 + col
+            if values[i] == 0:
+                row_str += " "
+            elif values[i] == 1:
+                row_str += "X"
+            elif values[i] == 2:
+                row_str += "O"
+            if col < 2:
+                row_str += "|"
+        print(row_str)
+        if row < 2:
+            print("-------")
 
 
 if __name__ == '__main__':
+    """parser = argparse.ArgumentParser()
+    parser.add_argument("numbers", type=int, help="Move to make")
+    args = parser.parse_args()"""
     main()
