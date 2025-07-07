f19c5ef3ae00614fb47edac365248324477b97ac
vlww <victorcalehr18@gmail.com>
2025-07-07 09:59:37 -0400

    make moves

--- practices/tictactoe.py
+++ practices/tictactoe.py
@@ -1,11 +1,14 @@
-import argparse
-
 def main():
     values = create_board()
     print_board(values)
+    num_turns = 0
+    while True:
+        values = make_move(values, num_turns)
+        num_turns += 1
+        print_board(values)
 
 def create_board():
-    values = [2, 0, 1, 0, 0, 2, 0, 0, 0]
+    values = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     return values
 
 def print_board(values):
@@ -25,9 +28,34 @@ def print_board(values):
         if row < 2:
             print("-------")
 
+valid_moves = {
+    "top left": 0, "top middle": 1, "top right": 2,
+    "middle left": 3, "center": 4, "middle right": 5,
+    "bottom left": 6, "bottom middle": 7, "bottom right": 8
+}
+
+def normalize_move(user_input):
+    return ' '.join(user_input.lower().strip().split())
+
+def make_move(values, turns):
+    while True:
+        user_input = input("Choose the position where you want to move: ")
+        move = normalize_move(user_input)
+
+        if move in valid_moves:
+            index = valid_moves[move]
+            if values[index] != 0:
+                print("That space is already taken. Try again.")
+            else:
+                player = 1 if turns % 2 == 0 else 2
+                values[index] = player
+                return values 
+        else:
+            print("Invalid move name. Try again (e.g., 'top left').")
+
+
+
+
 
 if __name__ == '__main__':
-    """parser = argparse.ArgumentParser()
-    parser.add_argument("numbers", type=int, help="Move to make")
-    args = parser.parse_args()"""
     main()
