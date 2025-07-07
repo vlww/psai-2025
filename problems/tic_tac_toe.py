33f9a75cb0b58ae540a7822f9fc0ac7d99d89e9d
vlww <victorcalehr18@gmail.com>
2025-07-07 14:34:04 -0400

    is terminal

--- problems/tic_tac_toe.py
+++ problems/tic_tac_toe.py
@@ -13,4 +13,30 @@ class TicTacToeProblem:
 
 
     def get_successors(self, state, player):
-        pass
+        states, actions = [], []
+        for move in self.get_moves(state,player):
+            new_state = state.copy()
+            new_state[move[0], move[1]] = player
+            states.append(new_state)
+            actions.append(move)
+        return states, actions
+    
+
+    def is_terminal(self, state):
+        for i in range(3):
+        #check each row
+            if all(state[i,:]==1) or all(state[i,:]==2):
+                return True
+        #check each column
+            if all(state[:,i]==1) or all(state[:,i]==2):
+                return True
+        #diagonals
+        flipped_diagonal = np.diag(np.fliplr(state))
+        if all(state.diagonal() == 1) or all(state.diagonal() == 2):
+            return True
+        if all(flipped_diagonal == 1) or all(flipped_diagonal == 2):
+            return True
+        #check for draw
+        if not any(state==0):
+            return True
+        return False
