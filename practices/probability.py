d423ea4f1bc848f226cca45706ceaa11adaca168
vlww <victorcalehr18@gmail.com>
2025-07-09 13:49:39 -0400

    double average

--- practices/probability.py
+++ practices/probability.py
@@ -1,25 +1,37 @@
+import random
+import matplotlib.pyplot as plt
+
 def spinner_world():
     probs = [0.5, 0.3, 0.2]
     outcomes = [1, 2, -1]
-    expected = 0
-
-    for p in range(len(probs)):
-        expected += probs[p]*outcomes[p]
-
+    expected = sum(p * o for p, o in zip(probs, outcomes))
     return expected
 
 def cursed_die():
-    probs = [1/6, 1/6, 1/6, 1/6, 1/6, 1/6]
+    probs = [1/6] * 6
     outcomes = [1, 2, 3, 4, 5, -3]
-
-    expected = 0
-
-    for p in range(len(probs)):
-        expected += probs[p]*outcomes[p]
-
+    expected = sum(p * o for p, o in zip(probs, outcomes))
     return expected
 
+def random_walker(trials):
+    positions = []
+    for i in range(trials):
+        position = 0
+        for j in range(1000):
+            if random.random() > 0.5:
+                position += 1
+            else:
+                position -= 1
+        positions.append(position)
+    return positions
 
 if __name__ == '__main__':
-    print(spinner_world())
-    print(cursed_die())
+    trials = 1000
+    positions = random_walker(trials)
+
+    plt.hist(positions, bins=30, edgecolor='black')
+    plt.title(f'plot')
+    plt.xlabel('position')
+    plt.ylabel('freq')
+    plt.grid(True)
+    plt.show()
