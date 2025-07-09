15e08361e4cb1069d2ad86c0621495e42c21ffec
vlww <victorcalehr18@gmail.com>
2025-07-09 15:21:35 -0400

    added matrix

---
+++ practices/wumpus_world.py
@@ -0,0 +1,13 @@
+class State:
+    def __init__(self, value):
+        self.value = value
+
+def main():
+    matrix = [[0, 0, 0, 100],
+              [0, -100, 0, 0],
+              [0, 0, -10, 0],
+              [0, 0, 0, 0]]
+
+
+if __name__ == '__main__':
+    main()
